82b743943fa1c605422789b336aee33f910f7419
uditknolder <86972851+uditknolder@users.noreply.github.com>
2021-08-06 01:55:42 +0530

    Update Main.java

--- clean_code_Assignment/Main.java
+++ clean_code_Assignment/Main.java
@@ -1,57 +1,71 @@
-public class Main //Main Class//
-{
-    static class CheckClean //static class//
-    {
-    
-    int SmallestNumber; //initializing variable//
-    int LargestNumber; 
+/**
+ * The type Main.
+ */
+public final class Main {
+    private Main() {
     }
-    
-    static CheckClean UpperBound_LowerBound(int GivenArray[], int SizeOfArray)  //Method to Find UpperBound And LowerBound//
-    {
+
+    /**
+     * The type Check clean.
+     */
+    static class CheckClean {
+        /**
+         *
+         */
+        private int smallestNumber;
+        /**
+         *
+         */
+        private int largestNumber;
+    }
+
+    /**
+     * Upper bound an lower bound check clean.
+     *
+     * @param givenArray the given array
+     * @param arraysize  the arraysize
+     * @return the check clean
+     */
+    static CheckClean upperBoundANLowerBound(final int[] givenArray, final int arraysize) {
         CheckClean objectOfCheck = new CheckClean();
-        
         int i;
-        
-        if (SizeOfArray == 1)
-        {
-            objectOfCheck.LargestNumber = GivenArray[0];
-            objectOfCheck.SmallestNumber = GivenArray[0];
+        if (arraysize == 1) {
+            objectOfCheck.largestNumber = givenArray[0];
+            objectOfCheck.smallestNumber = givenArray[0];
             return objectOfCheck;
         }
-        
-        if (GivenArray[0] > GivenArray[1]) 
-        {
-            objectOfCheck.LargestNumber = GivenArray[0];
-            objectOfCheck.SmallestNumber = GivenArray[1];
-        } 
-        else 
-        {
-            objectOfCheck.LargestNumber = GivenArray[1];
-            objectOfCheck.SmallestNumber = GivenArray[0];
+
+        if (givenArray[0] > givenArray[1]) {
+            objectOfCheck.largestNumber = givenArray[0];
+            objectOfCheck.smallestNumber = givenArray[1];
+        } else {
+            objectOfCheck.largestNumber = givenArray[1];
+            objectOfCheck.smallestNumber = givenArray[0];
         }
-        
-        for (i = 2; i < SizeOfArray; i++) 
-        {
-            if (GivenArray[i] > objectOfCheck.LargestNumber) 
-            {
-                objectOfCheck.LargestNumber = GivenArray[i];
-            }
-            else if (GivenArray[i] < objectOfCheck.SmallestNumber)
-            {
-                objectOfCheck.SmallestNumber = GivenArray[i];
+
+        for (i = 2; i < arraysize; i++) {
+            if (givenArray[i] > objectOfCheck.largestNumber) {
+                objectOfCheck.largestNumber = givenArray[i];
+            } else if (givenArray[i] < objectOfCheck.smallestNumber) {
+                objectOfCheck.smallestNumber = givenArray[i];
             }
         }
-    
+
         return objectOfCheck;
     }
-    
-    public static void main(String args[]) //Main method//
-    {
-        int GivenArray[] = {1000, 11, 445, 1, 330, 3000}; //declaring Array//
-        int SizeOfArray = 6;
-        CheckClean objectOfCheck = UpperBound_LowerBound(GivenArray, SizeOfArray); // passing value of array according to size//
-        System.out.printf("\n Smallest Number : %d", objectOfCheck.SmallestNumber);
-        System.out.printf("\n Largest Number : %d", objectOfCheck.LargestNumber);
+
+    /**
+     * Main.
+     *
+     * @param args the args
+     */
+    public static void main(final String... args) {
+        int[] givenArray; //declaring Array//
+        givenArray = new int[]{1, 11, 330, 445, 1000, 3000};
+        int sizeofarray = 6;
+        CheckClean objectOfCheck = upperBoundANLowerBound(givenArray, sizeofarray);
+        // passing value of array according to size//
+        System.out.printf("%n Smallest: %d", objectOfCheck.smallestNumber);
+        System.out.printf("%n Largest: %d", objectOfCheck.largestNumber);
     }
 }
